86aa7cb41bea30d23b9b8b0f4383f16e515a5b32
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2017-04-12 14:48:39 +0100

    Add JS for matching visitors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,6 +34,7 @@
 //= require modules/moj.emailPreview
 //= require modules/moj.multiSelect
 //= require modules/moj.tableSorter
+//= require modules/moj.matchVisitors
 
 (function() {
   'use strict';
